77c997f13b0d3b1eca84af371fc495bb8e3dc7f3
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-04-20 21:40:14 +0800

    Version 0.7.0

--- rebuild-wasi-decoder-openjp2.sh
+++ rebuild-wasi-decoder-openjp2.sh
@@ -4,4 +4,4 @@ cargo build --target wasm32-wasi --profile wasm32-wasi \
 	--no-default-features --features wasi-decoder,openjp2 \
 	--bin wasi-decoder
 
-cp ./target/wasm32-wasi/wasm32-wasi/wasi-decoder.wasm ./src/
+cp ./target/wasm32-wasi/wasm32-wasi/wasi-decoder.wasm ./src/wasi-decoder-openjp2.wasm
